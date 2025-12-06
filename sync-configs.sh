fdd722f53130c49b9a53bda994a4e2ab079db50e
tharris <tobyharris93@gmail.com>
2025-12-07 00:53:49 +1100

    feat: fix submodule + sync

--- sync-configs.sh
+++ sync-configs.sh
@@ -9,16 +9,15 @@ echo "Relinking stow-managed files for headless setup..."
 
 # MacOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    stow -R -v -n aerospace ghostty
+    stow -R -v aerospace ghostty
 fi
 
 # Linux
 # if [[ "$OSTYPE" == "darwin"* ]]; then
-#     stow -R -v -n hypr waybar ghostty
+#     stow -R -v hypr waybar ghostty
 # fi
 
-stow -R -v -n lazygit nvim tmux zsh
-
-
+# Platform agnostic
+stow -R -v git nvim tmux zsh lazygit
 
 echo "Done! 🎉"
