15104d5e2a013be6d314a22fa58ea82c8a7793b6
tharris <tobyharris93@gmail.com>
2026-06-22 17:57:30 +1000

    Update .config/nvim/lua/plugins/lualine.lua Update .config/nvim/lua/plugins/theme.lua Update .config/nvim/lua/plugins/treesitter.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,10 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+		dependencies = {
+			"catppuccin",
+			"nvim-tree/nvim-web-devicons",
+		},
 		opts = {
 			options = {
 				theme = "catppuccin",
