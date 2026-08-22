b4a7784f137d37b4abbc65037d51c1bafd18bb8d
tharris <tobyharris93@gmail.com>
2026-08-22 10:41:46 +1000

    feat: vim-aware ctrl+hjkl navigation between nvim splits and herdr panes
    - herdr: bind ctrl+hjkl to vim-herdr-navigation plugin actions
    - nvim: load the plugin's editor shim inside herdr, keep plain
      vim-tmux-navigator mappings elsewhere

--- dot_config/nvim/lua/plugins/tmux.lua
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -1,18 +1,29 @@
+-- Seamless split/pane navigation with ctrl+hjkl.
+-- Inside herdr: vim-herdr-navigation (vim-aware passthrough, falls back to
+-- tmux/wincmd on its own). Otherwise: plain vim-tmux-navigator mappings.
+local function herdr_nav_file()
+	local pattern = "~/.config/herdr/plugins/github/vim-herdr-navigation-*/editor/nvim.lua"
+	local matches = vim.fn.glob(pattern, true, true)
+	return matches[1]
+end
+
 return {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
-  },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  },
+	"christoomey/vim-tmux-navigator",
+	lazy = false,
+	init = function()
+		vim.g.tmux_navigator_no_mappings = 1
+	end,
+	config = function()
+		local nav_file = herdr_nav_file()
+		if nav_file then
+			dofile(nav_file)
+			return
+		end
+		local map = vim.keymap.set
+		map("n", "<c-h>", "<cmd>TmuxNavigateLeft<cr>")
+		map("n", "<c-j>", "<cmd>TmuxNavigateDown<cr>")
+		map("n", "<c-k>", "<cmd>TmuxNavigateUp<cr>")
+		map("n", "<c-l>", "<cmd>TmuxNavigateRight<cr>")
+		map("n", "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>")
+	end,
 }
